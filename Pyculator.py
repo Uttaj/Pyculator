949c1e212cbe1e43c3eb56a335e4c405f5539463
Uttaj <115397474+Uttaj@users.noreply.github.com>
2024-07-19 16:55:07 +0530

    Add files via upload

---
+++ Pyculator.py
@@ -0,0 +1,25 @@
+print("Welcome to Pyculator!")
+x=input("Which mathematical operation do you want to do? Press +,-,* and / accordingly: ")
+if x == "+":
+    a=int(input("Enter no.1: "))
+    b=int(input("Enter no.2: "))
+    print("The answer is ",a+b)
+elif x == "-":
+     c=int(input("Enter no.1: "))
+     d=int(input("Enter no.2: "))
+     print("The answer is ",c-d)
+elif x == "*":
+        e=int(input("Enter no.1: "))
+        f=int(input("Enter no.2: "))
+        print("The answer is ",e*f)
+elif x == "/":
+                g=int(input("Enter no.1: "))
+                h=int(input("Enter no.2: "))
+                print("The answer is ",g/h)
+
+            
+
+
+    
+
+    
